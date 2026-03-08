ca2c908a9b8f501a73b45ec450cd33f9b8d5b111
Geneviève Le Houx <gnvvlhx@gmail.com>
2026-03-07 20:39:21 -0500

    correct readme base url

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Hello
 
 ## Deployement
+
+``git add -f dist && git commit -m "new dist"``
+
 Push pages:
 
 ``git subtree push --prefix dist origin gh-pages``
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/bus-fare-optimizer/', // Replace with your repo name
+  base: 'https://github.com/genevieve-le-houx/bus-fare-optimizer', // Replace with your repo name
 })
